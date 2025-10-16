74c83367e782299f9e4a84bd5dd061d2022212e7
Edric Huang <Edric Huang>
2025-10-16 10:36:44 +0200

    Update structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const tasksRouter = require('./routes/tasks');
+const tasksRouter = require('./src/routes/tasks');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
