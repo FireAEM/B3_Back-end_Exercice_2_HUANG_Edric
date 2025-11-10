bdd6641956e130d1e785067286a13d323d562d7e
Edric Huang <Edric Huang>
2025-11-10 15:49:02 +0100

    Login rate limit implementation

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login, deleteUser } = require('../controllers/userController');
+const rateLimit = require('express-rate-limit');
+
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // 5 tentatives max par IP
+    message: `Trop de tentatives de connexion, réessayez plus tard.`,
+    standardHeaders: true,
+    legacyHeaders: false,
+})
 
 router.post('/signup', signup);
-router.post('/login', login);
+router.post('/login', loginLimiter, login);
 router.delete('/:id', deleteUser);
 
 module.exports = router;
